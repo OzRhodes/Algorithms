aae1fa8719b78296d135c66828e96f71f5f891bd
oz rhodes <oz@ozrhodes.com>
2023-02-22 01:23:11 +0000

    working algo

--- knightWalk.py
+++ knightWalk.py
@@ -29,44 +29,37 @@ How many steps does it take?
 def knightWalk(start, end):
     boardx = ['a','b','c','d','e','f','g','h']
     queue = []
-    visited =[]
+    visited = set()
     moves =[(1,2),(1,-2),(2,1),(2,-1),(-1,-2),(-1,2),(-2,-1),(-2,1)]
-    jumps = 1
 
     dest = (boardx.index(end[0]),end[1])
     start = (boardx.index(start[0]),start[1],0)
-    visited.append(start)
     queue.append(start)
-
+    count = 0
     while (queue):
-        currentLoc = queue.pop()
+        currentLoc = queue[0]
+        queue = queue[1:]
+
+        # check if at destination
+        
+        if currentLoc[0]==dest[0] and currentLoc[1]==dest[1]:
+            return f" Destination {end} reached in {currentLoc[2]} jumps" 
+        # add to visited
+        check = (currentLoc[0],currentLoc[1])
+        if check not in visited:
+            visited.add(check)
+   
         for i in range(8):
             # Calculate the possible list of next positions
-            newLoc = (currentLoc[0]+moves[i][0],currentLoc[1]+moves[i][1],jumps)
-            # check to see if you are at the destination
-            if newLoc[0]==dest[0] and newLoc[1] ==dest[1]:
-                return (f"Destination {dest} reached and it took {jumps} jumps")
+            newLoc = (currentLoc[0]+moves[i][0],currentLoc[1]+moves[i][1],currentLoc[2]+1)
             # If the possible steps are "off the board" ie <0 >7 for x or y then discount
-            if newLoc[0]>=0 and newLoc[0]<=7 and newLoc[1]>=0 and newLoc[1]<=7:
-                if newLoc not in visited:
-                    queue.append(newLoc)
-                    visited.append(newLoc)
-                    print(visited)
-            jumps +=1
+            if newLoc[0]>=0 and newLoc[0]<=7 and newLoc[1]>=0 and newLoc[1]<=7:            
+                queue.append(newLoc)
 
     return "Not possible to get to destination"
 
 
 
-
-    
-    return
-
-
-# Add the list of possile locations to a queue with the 'cost/step number'
-# step through the queue to see if the position is the destination and if not,
-# Add these positions to the queue
-
 start = ('d',4)
-end = ('h',3)
+end = ('g',0)
 print(knightWalk(start,end))
