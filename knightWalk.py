e8811dfecde86597c957d1259fdc95a220629be8
oz rhodes <oz@ozrhodes.com>
2023-02-22 16:26:23 +0000

    Added comments and changed the pop function for pop(0)

--- knightWalk.py
+++ knightWalk.py
@@ -30,21 +30,25 @@ def knightWalk(start, end):
     boardx = ['a','b','c','d','e','f','g','h']
     queue = []
     visited = set()
+    # Moves are the possible legal moves for the knight
     moves =[(1,2),(1,-2),(2,1),(2,-1),(-1,-2),(-1,2),(-2,-1),(-2,1)]
-
+# Convert the letter code to a number betweeen 0 & 7
     dest = (boardx.index(end[0]),end[1])
     start = (boardx.index(start[0]),start[1],0)
     queue.append(start)
     count = 0
     while (queue):
-        currentLoc = queue[0]
-        queue = queue[1:]
+
+        # G
+        # If there are items in the queueu get the first Item
+        currentLoc = queue.pop(0)
 
         # check if at destination
         
         if currentLoc[0]==dest[0] and currentLoc[1]==dest[1]:
             return f" Destination {end} reached in {currentLoc[2]} jumps" 
-        # add to visited
+
+        # add to visited, no need to store the jumps score
         check = (currentLoc[0],currentLoc[1])
         if check not in visited:
             visited.add(check)
@@ -57,8 +61,6 @@ def knightWalk(start, end):
                 queue.append(newLoc)
 
     return "Not possible to get to destination"
-
-
 
 start = ('d',4)
 end = ('g',0)
